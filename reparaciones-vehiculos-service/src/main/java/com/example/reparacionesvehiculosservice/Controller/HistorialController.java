e187529aa0d82a9b12f81f3cde843da7cec9e9f5
Niko085 <nicolas.gajardo.p@usach.cl>
2024-05-16 23:29:33 -0400

    Primer commit

---
+++ reparaciones-vehiculos-service/src/main/java/com/example/reparacionesvehiculosservice/Controller/HistorialController.java
@@ -0,0 +1,13 @@
+package com.example.reparacionesvehiculosservice.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/historialreparaciones")
+@CrossOrigin("*")
+public class HistorialController {
+}
