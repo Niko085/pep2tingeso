727c785822a427099f84cabc42f3bd8811a5e4fd
Niko085 <nicolas.gajardo.p@usach.cl>
2024-05-30 04:11:06 -0400

    Ahora creo que quedó bien el gateway

--- reparaciones-vehiculos-service/src/main/java/com/example/reparacionesvehiculosservice/Controller/HistorialController.java
+++ reparaciones-vehiculos-service/src/main/java/com/example/reparacionesvehiculosservice/Controller/HistorialController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/historial")
+@RequestMapping("/historialreparaciones")
 @CrossOrigin("*")
 public class HistorialController {
     @Autowired
